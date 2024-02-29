cc7b48ad3c3730a23e2a5b870505027e1d31371b
Ayush Chandola <155428667+potataahhh@users.noreply.github.com>
2024-02-29 12:35:41 +0530

    Create sol.cpp

---
+++ Array and String/Q7 == Best Time to Buy and Sell Stock/sol.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxi =0;
+        int mini = INT_MAX;
+
+        for(int i=0;i<n;i++){
+            mini = min(mini,prices[i]);
+
+            if(prices[i]>mini)
+            {
+                maxi = max(maxi,prices[i]-mini);
+            }
+        }
+        return maxi;
+    }
+};
